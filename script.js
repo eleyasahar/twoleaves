4cd18e95c58084db65b6294d8c5ca3ccc2e68cac
eleyasahar <eleyasahar143@gmail.com>
2026-05-21 22:09:38 +0530

    messege

---
+++ script.js
@@ -0,0 +1,22 @@
+// // var physics = Number(prompt('enter marks'))
+// // var maths = Number(prompt('enter marks'))
+// // var chemistry = Number(prompt('enter marks'))
+
+// // var avg = (maths + physics + chemistry) /3 
+// // console.log('avg', avg);
+
+// // if (avg >= 85){
+// //   console.log('A');
+// // }else if (avg>= 75){
+// //   console.log('B');
+// // }
+// //   else {
+// //   console.log('C');}
+
+// var gendeer = prompt('enter gender (M/F)')
+// var age = Number(prompt('enter age'))
+// if (gender == "F" && age >= 18 && age <= 60) {
+//   console.log('you will get ₹ 1500 per month');
+// } else {
+//   console.log('not allowed');
+// }
